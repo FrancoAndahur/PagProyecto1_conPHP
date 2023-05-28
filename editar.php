fca6c6a53f5788e583fcca8f1fa68f3393cd2314
FrancoAndahur <francoandahur60@gmail.com>
2023-05-28 19:16:38 -0400

    Creacion de crud basico para pagina usando php

---
+++ editar.php
@@ -0,0 +1,51 @@
+<?php 
+    include("conexion.php");
+    $con=conectar();
+
+$id=$_GET['id'];
+
+$sql="SELECT * FROM usuarios WHERE id='$id'";
+$query=mysqli_query($con,$sql);
+
+$row=mysqli_fetch_array($query);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+        <title>Editar</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+        
+    </head>
+    <body>
+                <div class="container mt-5">
+                    <form action="actualizar.php" method="POST">
+                    
+                                <input type="hidden" name="id" value="<?php echo $row['id']  ?>">
+                                
+                                <input type="text" class="form-control mb-3" name="nombre" placeholder="nombre" value="<?php echo $row['nombre']  ?>">
+                                <input type="text" class="form-control mb-3" name="apellido" placeholder="apellido" value="<?php echo $row['apellido']  ?>">
+                                <input type="text" class="form-control mb-3" name="usuario" placeholder="usuario" value="<?php echo $row['usuario']  ?>">
+                                <input type="text" class="form-control mb-3" name="correo" placeholder="correo" value="<?php echo $row['correo']  ?>">
+                                <input type="text" class="form-control mb-3" name="contraseña" placeholder="contraseña" value="<?php echo $row['contraseña']  ?>">
+                                <h6>Fecha de nacimiento:</h6>
+                                <input type="date" class="form-control mb-3" name="nacimiento" placeholder="Nacimiento" value="<?php echo $row['nacimiento']  ?>">
+                                <select class="form-select" name="genero" value="<?php echo $row['genero']  ?>">
+                                    <option selected>Selecciona tu género</option>
+                                    <option value="Masculino">Masculino</option>
+                                    <option value="Femenino">Femenino</option>
+                                    <option value="Otro">Otro</option>
+                                </select>
+                                <br>
+                                <input type="text" class="form-control mb-3" name="telefono" placeholder="telefono" value="<?php echo $row['telefono']  ?>">
+                                
+                                <input type="submit" class="btn btn-primary btn-block" value="Actualizar">
+                    </form>
+                    
+                </div>
+    </body>
+</html>
---
+++ usuarios.php
@@ -0,0 +1,90 @@
+<?php
+    include("conexion.php");
+    $con=conectar();
+
+    $sql="SELECT * FROM usuarios";
+    $query=mysqli_query($con,$sql);
+
+    $row=mysqli_fetch_array($query);
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pagina Usuarios</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+</head>
+<body>
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-md-2">
+                <h3>Ingrese datos</h3>
+                <form action="insertar.php" method="POST">
+                    <input type="text" class="form-control mb-3" name="nombre" placeholder="Nombre">
+                    <input type="text" class="form-control mb-3" name="apellido" placeholder="Apellido">
+                    <input type="text" class="form-control mb-3" name="usuario" placeholder="Usuario">
+                    <input type="text" class="form-control mb-3" name="correo" placeholder="Correo">
+                    <input type="text" class="form-control mb-3" name="contraseña" placeholder="Contraseña">
+                    <h6>Fecha de nacimiento:</h6>
+                    <input type="date" class="form-control mb-3" name="nacimiento" placeholder="Nacimiento">
+                    <select class="form-select" name="genero">
+                                <option selected>Selecciona tu género</option>
+                                <option value="Masculino">Masculino</option>
+                                <option value="Femenino">Femenino</option>
+                                <option value="Otro">Otro</option>
+                    </select>
+                    <br>
+                    <input type="text" class="form-control mb-3" name="telefono" placeholder="Teléfono">
+                    <input type="submit" class="btn btn-primary">
+                </form>
+            </div>
+            <div class="col-md-9">
+                <table class="table">
+                    <thead class="table-success table-striped">
+                        <tr>
+                            <th>Id</th>
+                            <th>Nombre</th>
+                            <th>Apellido</th>
+                            <th>Usuario</th>
+                            <th>Correo</th>
+                            <th>Contraseña</th>
+                            <th>Nacimiento</th>
+                            <th>Género</th>
+                            <th>Teléfono</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <?php
+                            while($row=mysqli_fetch_array($query)){
+                        ?>
+                            <tr>
+                                <th><?php echo $row['id']?></th>
+                                <th><?php echo $row['nombre']?></th>
+                                <th><?php echo $row['apellido']?></th>
+                                <th><?php echo $row['usuario']?></th>
+                                <th><?php echo $row['correo']?></th>
+                                <th><?php echo $row['contraseña']?></th>
+                                <th><?php echo $row['nacimiento']?></th>
+                                <th><?php echo $row['genero']?></th>
+                                <th><?php echo $row['telefono']?></th>
+
+                                <th><a href="editar.php?id=<?php echo $row['id'] ?>" class="btn btn-info">Editar</a></th>
+                                <th><a href="eliminar.php?id=<?php echo $row['id'] ?>" class="btn btn-danger">Eliminar</a></th> 
+                            </tr>
+                        <?php
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
